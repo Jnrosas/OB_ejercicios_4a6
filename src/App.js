e11a293a02f1d001b8819504a9d5a121ec0bb90d
Juan Rosas <rosas.juan@live.com>
2022-07-05 14:56:32 -0300

    Ej_ob_4a6

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+// import ClockC from './ClockC';
+import ClockF from './ClockF';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <ClockC /> */}
+        <ClockF />
       </header>
     </div>
   );
---
+++ src/ClockF.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+
+const ClockF = () => {
+
+  const [state, setState] = useState({
+    fecha: new Date(),
+    edad: 0,
+    nombre: 'Martín',
+    apellidos: 'San José'
+  });
+
+  useEffect(() => {
+    setInterval (
+      () => {
+        setState((prevState) => {
+          let edad = prevState.edad +0.5;
+          return {
+             ...prevState,
+             fecha: new Date(),
+             edad
+          }
+        });
+      }, 1000
+   );
+  
+    return () => {
+      clearInterval (setInterval);
+    };
+  }, []);
+
+  return (
+    <div>
+      <h2>Hora Actual:{state.fecha.toLocaleTimeString()}</h2>
+      <h3>{state.nombre} {state.apellidos}</h3>
+      <h1>Edad: {state.edad}</h1>
+
+    </div>
+  );
+}
+
+export default ClockF;
